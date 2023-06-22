ac02bf71765ba74037d072d5084e088ebbc39b9c
BongaDigiLink <bonga@digilink.africa>
2023-06-22 11:53:29 +0200

    login development. dashboard UI elements interface need to be removed around the login element

--- Angular_Project/src/app/app-routing.module.ts
+++ Angular_Project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResetComponent } from './Layouts/user/reset/reset.component';
+import { CloginComponent } from './Layouts/user/login/clogin.component';
 
-const routes: Routes = [
+const routes: Routes = 
+[
+  {
+    path:"reset", component:ResetComponent
+  },
+  {
+    path:'login', component: CloginComponent
+  },
   {
     path:'home',
     loadChildren: () => import('./Layouts/home/home.module').then(m => m.HomeModule)
--- Angular_Project/src/app/app.module.ts
+++ Angular_Project/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CfooterComponent } from './Layouts/cfooter/cfooter.component';
 import { CheaderComponent } from './Layouts/cheader/cheader.component';
 import { UsersModule } from './Layouts/users/users.module';
+import { ResetComponent } from './Layouts/user/reset/reset.component';
+import { CloginComponent } from './Layouts/user/login/clogin.component';
+import { UserDashboardComponent } from './Layouts/user/user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CfooterComponent,
     CheaderComponent,
+    ResetComponent,
+    UserDashboardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UsersModule
+    UsersModule,
+    RouterModule.forRoot([
+      {path:'login', component:CloginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
