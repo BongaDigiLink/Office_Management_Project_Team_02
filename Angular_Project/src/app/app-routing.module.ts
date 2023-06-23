2c8bc439ff5f1049bf663d54a4f08f92ff148290
BongaDigiLink <bonga@digilink.africa>
2023-06-23 11:47:59 +0200

    comments for adding new components and their routes under the dashboard module.

--- Angular_Project/src/app/app-routing.module.ts
+++ Angular_Project/src/app/app-routing.module.ts
@@ -12,6 +12,30 @@ const routes: Routes =
     path:'login', component: CloginComponent
   },
   {
+    path:'', component:CloginComponent
+  },
+  /**After logging in user is directed to dashboard path (dashboard module)
+   * The Dashboard Module has the following paths
+   * Admin:
+    *   localhost:4200/dashboard/bookings
+    *   localhost:4200/dashboard/assets
+    *   localhost:4200/dashboard/users
+    *   localhost:4200/dashboard/inventory
+    *   localhost:4200/dashboard/meetings
+    * 
+    * User/Candidate: Not Completed Yet
+    *   localhost:4200/dashboard        - default home, display summary of available functions (1,2,3)
+    *   localhost:4200/dashboard/---
+    *   localhost:4200/dashboard/---
+    *   localhost:4200/dashboard/---
+    *   localhost;4200/dashboard/profile
+    * 
+    *   To add other components and their routes
+    *   add the components on : declarations  (file: dashboard.module.ts)
+    *   Check the file: dashboard-routing.module.ts 
+    *   and edit.
+   * **/
+  {
     path:'dashboard', loadChildren: () => 
     import('./Layouts/dashboard/dashboard.module').then( m => m.DashboardModule)
   }
