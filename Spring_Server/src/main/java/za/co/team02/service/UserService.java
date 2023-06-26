262388402e6ef7a459b848b1082f6e1523afabbc
Thulibathi <nokuthulamargaret@gmail.com>
2023-06-26 11:48:40 +0200

    register

--- Spring_Server/src/main/java/za/co/team02/service/UserService.java
+++ Spring_Server/src/main/java/za/co/team02/service/UserService.java
@@ -1,27 +1,28 @@
 package za.co.team02.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import za.co.team02.dao.UserDAO;
 import za.co.team02.model.SiteUser;
+import za.co.team02.repository.UserRepository;
 
+@Service
 public class UserService
 {
-
     @Autowired
-    private UserDAO userDAO;
-
+    private UserRepository userRepository;
     /**
      * If user email not unique return null.
      * @param newUser - new user object, add to datastore
      * @return - the added user object
      */
-    public SiteUser addUser(SiteUser newUser)
-    {
-        if(userDAO.existsByEmail(newUser.getEmail()))
-        {
-            return null;
+    public boolean addUser(SiteUser newUser) {
+        boolean isSaved = false;
+        int result = userRepository.saveUser(newUser);
+        if(result>0) {
+            isSaved = true;
         }
-        return userDAO.save(newUser);
+        return isSaved;
     }
 
 }
