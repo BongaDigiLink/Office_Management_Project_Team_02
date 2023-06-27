c204f9463ba5960d5c182a3473f3b6684d2bbac6
BongaDigiLink <bonga@digilink.africa>
2023-06-27 15:56:01 +0200

    get all users

--- Angular_Project/src/app/Layouts/dashboard/users/user.component.ts
+++ Angular_Project/src/app/Layouts/dashboard/users/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AdminService } from 'src/app/service/admin.service';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +10,19 @@ import { Component, OnInit } from '@angular/core';
 
 export class UserComponent implements OnInit{
 
-  constructor()
-  {
+  constructor(private service: AdminService)
+  {}
 
-  }
+  all_users!: User[];
 
   ngOnInit(): void {
-    
+    this.service.getUsers().subscribe((all_users) => 
+    {
+      //View a list of users:
+      console.log("All users")
+      console.log(all_users);
+      all_users = all_users;
+    } )
   }
 
 }
