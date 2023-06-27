9d1369c75011f293da5de1732c110fb4549e8c29
BongaDigiLink <bonga@digilink.africa>
2023-06-27 14:05:10 +0200

    UI fix

--- Angular_Project/src/app/Layouts/dashboard/home/index.component.ts
+++ Angular_Project/src/app/Layouts/dashboard/home/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
+  constructor(private service: AuthService){}
+
+  admin_interface : boolean = false;
+  user_interface : boolean = false;
+
   ngOnInit(): void 
   {
-    
+    this.service.userType$.subscribe( (value) => 
+    {
+      this.setInterfaces(value)
+    })
   }
 
+  /**
+   * @param value 
+   */
+  setInterfaces(value: number)
+  {
+    if(value === 1)
+    {
+      this.admin_interface = true;
+    }
+    else if(value === 2)
+    {
+      this.user_interface = true;
+    }
+    else
+    {
+      //do nothing.
+      this.user_interface = true;
+    }
+  }
 }
