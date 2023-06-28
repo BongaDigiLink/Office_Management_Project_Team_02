da23be7789223f982e34d1c8833b0db6c3b40528
BarnabasDigiLink <130969883+BarnabasDigiLink@users.noreply.github.com>
2023-06-28 15:46:03 +0200

    updates

--- Angular_Project/src/app/Layouts/dashboard/users/user.component.ts
+++ Angular_Project/src/app/Layouts/dashboard/users/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { AdminService } from 'src/app/service/admin.service';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-user',
@@ -10,7 +12,7 @@ import { AdminService } from 'src/app/service/admin.service';
 
 export class UserComponent implements OnInit{
 
-  constructor(private service: AdminService)
+  constructor(private service: AdminService, private dialog: MatDialog)
   {}
 
   all_users!: User[];
@@ -23,6 +25,31 @@ export class UserComponent implements OnInit{
       console.log(all_users);
       all_users = all_users;
     } )
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '50%',
+      height: '',
+      position: {
+        top: '-40vh',
+        left: '30vw'
+    }
+    });
+  }
+
+  OpenModal(): void {
+    const overlay = document.getElementById('popup1')!
+    overlay.style.display = 'block'
+    const modal = document.getElementById('modal')!
+    modal.style.display = 'block'
+  }
+
+  CloseModal(): void {
+    const overlay = document.getElementById('popup1')!
+    overlay.style.display = 'none'
+    const modal = document.getElementById('modal')!
+    modal.style.display = 'none'
   }
 
 }
