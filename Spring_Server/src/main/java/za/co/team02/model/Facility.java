faa5aac206419d289e899e817f24b358b787c161
PhemeloMotsamai <phemelojmotsamai@gmail.com>
2023-06-30 14:15:21 +0200

    added facility end point for user role

--- Spring_Server/src/main/java/za/co/team02/model/Facility.java
+++ Spring_Server/src/main/java/za/co/team02/model/Facility.java
@@ -1,23 +1,76 @@
-//package za.co.team02.model;
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import java.time.LocalDateTime;
-//
-//@Entity(name = "facility")
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Facility {
-//
-//    @Column(name = "room_id")
-//    private int trainingRoomId;
-//
-//    @Column(name = "room_name")
-//    private String trainingRoomName;
-//
+package za.co.team02.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+
+@Entity(name = "facility")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Facility   {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "room_name")
+    private String trainingRoomName;
+
+    @Temporal(TemporalType.DATE)
+    Date date;
+
+    @Column (name = "start-time")
+    private String startTime;
+
+    @Column (name = "end-time")
+    private String endTime;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTrainingRoomName() {
+        return trainingRoomName;
+    }
+
+    public void setTrainingRoomName(String trainingRoomName) {
+        this.trainingRoomName = trainingRoomName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
 //    @Column(name = "room_type")
 //    private String trainingRoomType;
 //
@@ -26,4 +79,4 @@
 //
 //    @Column(name = "time")
 //    private LocalDateTime time;
-//}
+}
