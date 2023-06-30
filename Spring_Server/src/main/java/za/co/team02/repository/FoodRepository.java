7cac615209b4c2cbf7a0b32f174cfe30b7d6b6d2
Thulibathi <nokuthulamargaret@gmail.com>
2023-06-30 15:54:36 +0200

    Fixes

--- Spring_Server/src/main/java/za/co/team02/repository/FoodRepository.java
+++ Spring_Server/src/main/java/za/co/team02/repository/FoodRepository.java
@@ -1,7 +1,19 @@
 package za.co.team02.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import za.co.team02.model.FoodSupply;
+import java.util.Collection;
 
+@Repository
 public interface FoodRepository extends JpaRepository<FoodSupply,Integer> {
+//    @Query("SELECT * FROM Food_Supplies f WHERE f.Food_id = 9 ")
+//@Query("SELECT * FROM Food_Supplies f WHERE f.id = ? ")
+//   Collection<FoodSupply> getFoodByCohort(int cohortId);
+@Query("SELECT COUNT(*) FROM Food_Supplies")
+Collection<FoodSupply> getAllNoodles(int cohortId);
+
+@Query("SELECT COUNT(*) FROM Food_Supplies")
+Collection<FoodSupply> getAllOats(int cohortId);
 }
